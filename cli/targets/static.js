35ca75c3d6b4328c0cbb24adc78021ffeee75e72
Caleb Gregory <calebgregory@gmail.com>
2017-08-30 13:13:30 -0500

    name -> escapeName

--- cli/targets/static.js
+++ cli/targets/static.js
@@ -764,7 +764,7 @@ function buildClient(ref, service) {
         ]);
         push(escapeName(service.name) + "Client.prototype" + util.safeProp(lcName) + " = function " + escapeName(lcName) + "(request, callback) {");
             ++indent;
-            push("return this.rpcCall(\"" + name(method.name) + "\", $root" + method.resolvedRequestType.fullName + ", $root" + method.resolvedResponseType.fullName + ", request, callback);");
+            push("return this.rpcCall(\"" + escapeName(method.name) + "\", $root" + method.resolvedRequestType.fullName + ", $root" + method.resolvedResponseType.fullName + ", request, callback);");
             //push("return this.rpcCall(\"" + escapeName(lcName) + "\", $root." + exportName(method.resolvedRequestType) + ", $root." + exportName(method.resolvedResponseType) + ", request, callback);");
             --indent;
         push("};");
